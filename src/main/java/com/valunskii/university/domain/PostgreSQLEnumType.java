efef1bce7738552998ace5e1a8c6eabf02d2f967
vvalunskiy <vvalunskii@c-i-p.ru>
2019-08-21 18:37:51 +0300

    refact PostgreSQLEnumType

--- src/main/java/com/valunskii/university/domain/PostgreSQLEnumType.java
+++ src/main/java/com/valunskii/university/domain/PostgreSQLEnumType.java
@@ -1,20 +1,20 @@
 package com.valunskii.university.domain;
 
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
 
-import org.hibernate.HibernateException;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
-
 public class PostgreSQLEnumType extends org.hibernate.type.EnumType {
-    
+
+    @Override
     public void nullSafeSet(
             PreparedStatement st, 
             Object value, 
             int index, 
             SharedSessionContractImplementor session) 
-        throws HibernateException, SQLException {
+        throws SQLException {
         if(value == null) {
             st.setNull( index, Types.OTHER );
         }
